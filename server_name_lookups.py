e13ed0a74ff90c1e797600cccfac9aaefb1e4035
Abdul Rafay Khan <abdul.rafay.khan10@gmail.com>
2018-05-30 17:08:25 +0500

    Nothing too scary

---
+++ server_name_lookups.py
@@ -0,0 +1,9 @@
+import socket
+
+print(socket.gethostbyaddr("8.8.8.8"))
+print(socket.gethostbyname("www.google.com"))
+print(socket.gethostbyname("www.github.com"))
+print(socket.gethostbyname("www.python.org"))
+print(socket.gethostbyname("www.youtube.com"))
+print(socket.gethostbyname("www.myspace.com"))
+print(socket.gethostbyname("www.spotify.com"))
